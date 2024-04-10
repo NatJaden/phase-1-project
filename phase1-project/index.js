9fa5e1df469950461b8d3bc16e9a53224dac3e47
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-04-10 17:18:11 +0300

    some js

--- phase1-project/index.js
+++ phase1-project/index.js
@@ -10,7 +10,7 @@ function newDriver(e) {
     dateOfBirth: e.target.dateOfBirth.value,
     nationality: e.target.nationality.value,
     imageUrl: e.target.imageUrl.value,
-    upvotes: 0
+    upvotes: 0,
   };
   renderOneDriver(newDriverObj);
   addNewDriver(newDriverObj);
@@ -20,48 +20,26 @@ function renderOneDriver(driver) {
   const card = document.createElement("li");
   card.className = "card";
   card.innerHTML = `
-      <img src ="${driver.imageUrl}">
-      <div class='content'>
+  <div class="image-container">
+      <img src ="${driver.imageUrl}" class = "driver-image">
+      </div>
+     <p> <span class= "upVotes"> ${driver.upvotes}<span> Upvotes </p>
+      <div class="content">
         <h4>${driver.givenName} ${driver.familyName}</h4>
         <p>Driver ID: ${driver.driverId}</p>
-        <p>Wikipedia URL: ${driver.url}</p>
+        <p>Wikipedia URL:${driver.url}</a></p>
         <p>Date of Birth: ${driver.dateOfBirth}</p>
         <p>Nationality: ${driver.nationality}</p>
       </div>
       <div class="Buttons"><button id="delbtn"> Delete </button> 
-      <button id="editbtn"> Edit </button>
+      <button id="editbtn"> Upvote </button> 
       </div>
     `;
 
-  card
-    .querySelector("#editbtn")
-    .addEventListener("click", () => {
-      card
-    });
-  document.querySelector(".driver-list").appendChild(card);
-}
-function getAllDrivers() {
-  fetch("http://localhost:3000/drivers")
-    .then((res) => res.json())
-    .then((drivers) => {
-      drivers.forEach((driver) => renderOneDriver(driver));
-    });
-}
 
-function addNewDriver(newDriverObj) {
-  fetch("http://localhost:3000/drivers", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newDriverObj),
-  })
-    .then((res) => res.json())
-    .then((driver) => console.log(driver));
+  card.querySelector("#editbtn").addEventListener("click", () => {
+    driver.upvotes++;
+    card.querySelector("span").textContent = driver.upvotes;
+    upvoteDriver(driver);
+  });
 }
-
-function initialize() {
-  getAllDrivers();
-}
-
-initialize();
